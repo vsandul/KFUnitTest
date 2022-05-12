ac1bc09358ae4b05ad855d4ccef8fa9c9ea67f95
vsandul <vladislav.sandul@cern.ch>
2022-05-12 10:32:47 +0200

    J/Psi -> e+ e- decay wasd added (scripts to download geant4 program and MC data)

---
+++ GeantPrograms/download_program_JPsi_to_eplus_eminus.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -O JPsi_to_eplus_eminus.tar.gz https://cernbox.cern.ch/index.php/s/hoUlF0WR2UBjccq/download
---
+++ MCData/JPsi_to_eplus_eminus/download_data_JPsi_to_eplus_eminus.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget -O JPsi_to_eplus_eminus_output.root https://cernbox.cern.ch/index.php/s/x7FsSz5cab4Mj3t/download
